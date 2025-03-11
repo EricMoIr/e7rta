a66fe5781d589b2bc9278a65aebbbe9e9ede845b
Eric Mordezki <ericm@velotix.ai>
2025-03-11 13:00:02 -0300

    Added the circular border to the drafts

--- client/src/pages/HomePage/PlayerDraft/index.tsx
+++ client/src/pages/HomePage/PlayerDraft/index.tsx
@@ -1,19 +1,28 @@
 import { FC, useMemo } from "react";
 import ConditionalWrapper from "../../../components/Wrapper";
 import HeroCard from "../HeroCard";
+import "./index.scss";
+import classNames from "classnames";
 
-const PlayerDraft: FC<{ isFirstPick: boolean, onFirstPickChange: () => void, heroes: Hero[], isMine?: boolean }> = ({ isFirstPick, heroes, isMine = false }) => {
-  const slots = useMemo(() => [...Array(5)], [])
+const PlayerDraft: FC<{
+  isFirstPick: boolean;
+  onFirstPickChange: () => void;
+  heroes: Hero[];
+  isMine?: boolean;
+}> = ({ isFirstPick, heroes, isMine = false }) => {
+  const slots = useMemo(() => [...Array(5)], []);
   return (
     <div>
-    <ConditionalWrapper condition={isFirstPick} wrapper={"b"}>
-      First pick
-    </ConditionalWrapper>
-    <div>
-      {slots.map((_, i) => (<HeroCard key={i} hero={heroes && heroes[i]} isMine={isMine} />))}
+      <ConditionalWrapper condition={isFirstPick} wrapper={"b"}>
+        First pick
+      </ConditionalWrapper>
+      <div className={classNames({ "is-mine": isMine, "is-theirs": !isMine })}>
+        {slots.map((_, i) => (
+          <HeroCard key={i} hero={heroes && heroes[i]} isMine={isMine} />
+        ))}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
 export default PlayerDraft;
